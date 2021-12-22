c691797121345f4f818aac196e0fab444e498928
Your Name <alexander.klashitsky@gmail.com>
2021-12-22 15:14:07 +0200

    test

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,10 +1,15 @@
+import { moduleUtil } from `./util.services.js`
+import { moduleStorage } from `./storage.services.js`
+
 export const locService = {
     getLocs
 }
+const KEY = locDB
+
 
 
 const locs = [
-    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 }, 
+    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 },
     { name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
 ]
 
@@ -16,4 +21,18 @@ function getLocs() {
     });
 }
 
+
+function createLocs(name, lat, lng, weather) {
+    const loc = {
+        id: makeID(),
+        name,
+        lat,
+        lng,
+        weather: 'nice',
+        createAt: Date.now(),
+        modifiedAt: Date.now()
+    }
+    locs.push
+
+}
 
